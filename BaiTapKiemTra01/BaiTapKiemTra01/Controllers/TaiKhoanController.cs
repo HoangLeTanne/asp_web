279c67ed99e8cf255e5c9659887c1a70603585b5
hoangletan <tanlehoang0211@gmail.com>
2024-09-10 15:06:24 +0700

    updates

---
+++ BaiTapKiemTra01/BaiTapKiemTra01/Controllers/TaiKhoanController.cs
@@ -0,0 +1,30 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel model)
+        {
+            if (model.Username != null )
+            {
+                return Content(model.Username);
+            }
+            return View(model);
+        }
+        public IActionResult BaiTap2()
+        {
+            var sanpham = new SanPhamViewModel();
+            {
+
+            }
+            return View(sanpham);
+        }
+
+    }
+}
---
+++ BaiTapKiemTra01/BaiTapKiemTra01/Models/SanPhamViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BaiTapKiemTra01.Models
+{
+    public class SanPhamViewModel
+    {
+        public int Id { get; set; }
+        public string TenSanPham { get; set; }
+        public string GiaBan { get; set; }
+       
+       
+    }
+}
